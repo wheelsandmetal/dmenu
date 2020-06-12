e5f5c171d3d0c41b2c47089b550513f61f01b1f8
wheelsandmetal <jakob@schmutz.co.uk>
2020-06-12 23:47:32 +0100

    patch: center

--- config.h
+++ config.h
@@ -3,6 +3,8 @@
 
 static int instant = 1;
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int centered = 1;                    /* -c option; centers dmenu on screen */
+static int min_width = 500;                 /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"monospace:size=10"
